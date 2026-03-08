e6cad46f16db2a2d1e91202126beb73ff950b150
antares <antares@alyr.dev>
2026-03-08 14:46:14 +0800

    0.15.0

--- package.nix
+++ package.nix
@@ -11,12 +11,12 @@ let
   };
   # The SHA256 hashes for the IronClaw releases for each platform. Update these when a new release is made.
   sha256Map = {
-    "x86_64-linux" = "sha256:027vi6w61wsnmip1xmpcc0nlmb0km9h8zs26ip28s0rspb06lcxg";
-    "aarch64-linux" = "sha256:151qlc5mfchzzracky2wflzvdj7nip52cm3my46yv0292gk03c37";
-    "aarch64-darwin" = "sha256:0pgpawkadgizs8kkl0l6zcn8jiclnd74frdk14bh09by2sj00jcl";
+    "x86_64-linux" = "sha256:126ksw01b3qrhhnkbgk80j8azf661y2cb9c2g2rqyn6pnkqw5pj6";
+    "aarch64-linux" = "sha256:170cdgzyrpib3bj5wjk8p9nsc6s441dz0kp7m8689c74mkfyg7p0";
+    "aarch64-darwin" = "sha256:0s8ia0by68rgxgkssxsqw6xh4axvi6qxdasjcicqa7x5kvyhh08g";
   };
   # The version of IronClaw to package. Update this when a new release is made.
-  version = "0.16.1";
+  version = "0.15.0";
   # The system for which to fetch the release. This is determined dynamically based on the host platform.
   system = stdenv.hostPlatform.system;
   src = fetchTarball {
